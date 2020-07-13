15a7c5d765a65192aa094d93e35ef1bc3b5383d4
ka-sumiya <ka-sumiya@dgic.co.jp>
2020-07-13 15:32:55 +0900

    炭谷の作成したAPIを追加

---
+++ FjiwaraTest/src/main/java/com/example/demo/data/TestBook.java
@@ -0,0 +1,22 @@
+package com.example.demo.data;
+
+public class TestBook {
+
+	private String book_name;
+
+	public String getBook_name() {
+		return book_name;
+	}
+
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+
+	@Override
+	public String toString() {
+		return "TestBook [book_name=" + book_name + "]";
+	}
+
+
+
+}
